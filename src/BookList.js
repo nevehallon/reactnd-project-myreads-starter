9c1dc3db31eee2516dbcf2230580e9a890d7a432
Neveh Allon <NevehAllon@gmail.com>
2018-07-24 21:34:11 +0300

    New component

---
+++ src/BookList.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types'
+import Fishelf from './Fishelf'
+
+class BookList extends Component {
+    static propTypes = {
+      books: PropTypes.array.isRequired,
+      switchGroup: PropTypes.func.isRequired
+    }
+
+
+    render() {
+        const { books, switchGroup } = this.props
+
+        const groupTypes = 
+            [{ type: 'currentlyReading', name: 'Currently Reading' },
+             { type: 'wantToRead',  name: 'Want to Read' },
+             { type: 'read', name: 'Read'}]
+             
+
+        return (
+
+        <div className='book-list-content'>
+        {groupTypes.map((group, i) => {
+            const groupie = books.filter( book => book.group === group.type)
+            return (
+                <div className='group' key={i}>
+                <h2 className='group-name'>{group.name}</h2>
+                <div className='group-books'>
+
+                <Fishelf
+                  books={ groupie }
+                  switchGroup={ switchGroup }
+                />
+
+                </div>
+                </div> )
+        })}
+        </div>
+        )
+    }
+}
+
+export default BookList
