fe8089f765d116e2af1df432f6eb21a0d1d75b6a
Neveh Allon <NevehAllon@gmail.com>
2018-08-13 12:37:16 +0300

    Working App

--- src/Search.js
+++ src/Search.js
@@ -39,7 +39,7 @@ class Search extends Component {
       return (
         <div className="search-books">
           <div className="search-books-bar">
-            <Link basename={process.env.PUBLIC_URL} className="close-search"  to="/">Close</Link>
+            <Link path={process.env.PUBLIC_URL + '/'} className="close-search"  to="/">Close</Link>
             <div className="search-books-input-wrapper">
               <input type="text"
                 placeholder="Search by title or author"
