2c6fc573e062aeb7fd9264e80675e73b7f80bd77
Neveh Allon <NevehAllon@gmail.com>
2018-08-13 12:31:49 +0300

    Working App

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
-ReactDOM.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}><App /></BrowserRouter>,
-  document.getElementById('root')
-);
+ReactDOM.render((
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
+  <App />
+</BrowserRouter>),
+  document.getElementById('root'));
